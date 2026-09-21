1c27a607ebe906c389f9a49c27be038eeb63cb30
20177156 <20177156@tafe.wa.edu.au>
2026-09-21 18:29:20 +0800

    feature: adding user input feature to allow them to enter their name

--- example.py
+++ example.py
@@ -1,4 +1,10 @@
 from random import randint 
+while True:
+    name  = input("Please enter your name: \n")
+    try:
+        float(name)
+    except ValueError:
+        break
 
 number = randint(0,20)
-print("Hello!!! " * number)
+print(f"Hello {name.upper()}!!! " * number)
