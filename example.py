1303af22acfd9f2a5874d5cac7e78005a05e13c0
Amira <20177156@tafe.wa.edu.au>
2026-09-21 18:56:06 +0800

    feature: makes a judgemental declarative based on user inputted age (#5)

--- example.py
+++ example.py
@@ -3,8 +3,24 @@ while True:
     name  = input("Please enter your name: \n")
     try:
         float(name)
+        print("Please enter a name, not a number!")
     except ValueError:
         break
 
-number = randint(0,20)
-print(f"Hello {name.upper()}!!! " * number)
+while True:
+    age = input("What's your age? \n")
+    try:
+        age = int(age)
+        break
+    except ValueError:
+        print("You must enter your age as a numerical integer.")
+
+number = randint(0,10)
+
+if age <= 25:
+    declaritive = "you're young and cool"
+    number = round( (age + number) / 2)
+else:
+    declaritive = "you're old and yucky"
+    
+print(f"Hello {name.upper()}, {declaritive}!!! " * number)
