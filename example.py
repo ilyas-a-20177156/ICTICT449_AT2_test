fc1ed7791874313cc3dfdbee35ea9e408ca92db3
Amira <20177156@tafe.wa.edu.au>
2026-09-22 17:28:06 +0800

    Feature/password guesser (#6)
    * feat: implement secret word and also updated name check code to be more specific
    
    * feature: implementation of function that ensures user inputted guess contains only letters and is the right length
    
    * feature: all the required sentences to implement the workflow
    
    * feature: final implementation of password guessing game

--- example.py
+++ example.py
@@ -1,10 +1,21 @@
 from random import randint 
+
+def password_checker(question, secret_word):
+    while True:
+        guess = input(question)
+        if guess.isalpha() == False:
+            print("Sorry, that is not an acceptable guess. Try again")
+        elif len(guess) != len(secret_word):
+            print(f"Sorry, that guess must be {len(secret_word)} letters long.")
+        else:
+            break
+    return guess.lower()
+
 while True:
     name  = input("Please enter your name: \n")
-    try:
-        float(name)
-        print("Please enter a name, not a number!")
-    except ValueError:
+    if name.isalpha() == False:
+        print("No name contains those characters!")
+    else:
         break
 
 while True:
@@ -20,7 +31,37 @@ number = randint(0,10)
 if age <= 25:
     declaritive = "you're young and cool"
     number = round( (age + number) / 2)
+    descriptor = "cool"
 else:
     declaritive = "you're old and yucky"
+    descriptor = "yucky"
     
 print(f"Hello {name.upper()}, {declaritive}!!! " * number)
+descriptor_name = f"{descriptor.capitalize()} {name.upper()}"
+
+secret_word = "aorta"
+
+print(f"Ok {descriptor_name}, please guess what is the 5 letter password. \nYou'll have 5 tries")
+intro_sentence = f"{descriptor_name}, please enter your first guess: \n"
+subsequent_sentence = f"{descriptor_name}, please enter another guess: \n"
+
+hints = [
+    "Hint: it's in the body\n",
+    "Hint: it carries oxygenated blood\n",
+    "Hint: it's a major blood vessel\n",
+    "Hint: its connected to the heart\n",
+    "Oh no, you're out of tries! \nBetter luck next time! :("
+]
+
+for attempt in range(0,5):
+    if attempt == 0:
+        guess = password_checker(intro_sentence, secret_word)
+    else:
+        guess = password_checker(subsequent_sentence, secret_word)
+
+    if guess == secret_word:
+        print("You've got it!")
+        break
+    else:
+        print("Sorry that's wrong!")
+        print(hints[attempt])
