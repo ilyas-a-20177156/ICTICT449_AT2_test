b53813f47313883cdeb76f3dc0ed2d7556a7b0c9
20177156 <20177156@tafe.wa.edu.au>
2026-09-22 17:26:04 +0800

    feature: final implementation of password guessing game

--- example.py
+++ example.py
@@ -2,14 +2,14 @@ from random import randint
 
 def password_checker(question, secret_word):
     while True:
-        password = input(question)
-        if password.isalpha() == False:
+        guess = input(question)
+        if guess.isalpha() == False:
             print("Sorry, that is not an acceptable guess. Try again")
-        elif len(password) != len(secret_word):
+        elif len(guess) != len(secret_word):
             print(f"Sorry, that guess must be {len(secret_word)} letters long.")
         else:
             break
-    return password
+    return guess.lower()
 
 while True:
     name  = input("Please enter your name: \n")
@@ -37,19 +37,31 @@ else:
     descriptor = "yucky"
     
 print(f"Hello {name.upper()}, {declaritive}!!! " * number)
-descriptor_name = f"{descriptor} {name.capitalize()}"
+descriptor_name = f"{descriptor.capitalize()} {name.upper()}"
 
 secret_word = "aorta"
 
 print(f"Ok {descriptor_name}, please guess what is the 5 letter password. \nYou'll have 5 tries")
-intro_sentence = f"{descriptor_name.capitalize()}, please enter your first guess"
-subsequent_sentence = f"{descriptor_name.capitalize()}, please enter another guess: \n"
+intro_sentence = f"{descriptor_name}, please enter your first guess: \n"
+subsequent_sentence = f"{descriptor_name}, please enter another guess: \n"
 
 hints = [
-    "Hint: it's in the body",
-    "Hint: it carries oxygenated blood",
-    "Hint: it's a major blood vessel",
-    "Hint: its connected to the heart"
-    "Sorry, you're out of tries! :("
+    "Hint: it's in the body\n",
+    "Hint: it carries oxygenated blood\n",
+    "Hint: it's a major blood vessel\n",
+    "Hint: its connected to the heart\n",
+    "Oh no, you're out of tries! \nBetter luck next time! :("
 ]
 
+for attempt in range(0,5):
+    if attempt == 0:
+        guess = password_checker(intro_sentence, secret_word)
+    else:
+        guess = password_checker(subsequent_sentence, secret_word)
+
+    if guess == secret_word:
+        print("You've got it!")
+        break
+    else:
+        print("Sorry that's wrong!")
+        print(hints[attempt])
